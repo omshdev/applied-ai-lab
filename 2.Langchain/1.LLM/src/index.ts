08bfa914f36afa158f5bee93a7a43aa50617c9a2
Om Sharma <omsharma.83173@gmail.com>
2025-09-03 17:12:00 +0530

    feat : created-a-manual-chain-like-workflow.

--- 2.Langchain/1.LLM/src/index.ts
+++ 2.Langchain/1.LLM/src/index.ts
@@ -1,67 +1,48 @@
-import {  HumanMessage,SystemMessage,AIMessage} from "@langchain/core/messages";
-import { ChatGoogleGenerativeAI} from "@langchain/google-genai";
-import { ChatPromptTemplate , MessagesPlaceholder} from "@langchain/core/prompts"
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai"
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+import { z } from "zod";
 
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 dotenv.config();
 
-const SYSTEM_TEMPLTE = `
-Your're an Om Sharma's Assistant!. the User will give you input {user_input} and will respond based on query. and You Have History of Messages {chatHistory} So You Will not Loose the Context What user is Chating To You.
 
-
-`
-
-
-const input = require('prompt-sync')({ sigint: true });
-
- const model = new ChatGoogleGenerativeAI({
+const model = new ChatGoogleGenerativeAI({
     apiKey : process.env.GOOGLE_API_KEY!,
     model : "gemini-2.0-flash",
     temperature : 0
 });
+const SYSTEM_TEMPLTE = `Your're an Good Topic Info Provider.`
+
+const template = ChatPromptTemplate.fromMessages([
+    ["system",SYSTEM_TEMPLTE],["human","{topic}"]
+])
+
+const SYSTEM_TEMPLTE2 = `Your'e an Summarizer for Given topic with five lines. You will cover all Topic stuff and summarize into five lines.`
+const summarizerToFiveLinetemplate = ChatPromptTemplate.fromMessages([
+    ["system",SYSTEM_TEMPLTE2],["user","{info}"]
+]);
 
 
-const chatHistory : any= [];
+async function chat(){
+    // const response = await model.invoke("What is Your Name ? "); 
+    // console.log(response.content) 
+    const promptValue = await template.invoke({ topic : "What is kafka ? " });
+    // console.log(promptValue);
 
-chatHistory.push(new SystemMessage(SYSTEM_TEMPLTE));
-
-(async ()=>{
-    while(true){
-        const user = input("User : ");
-        
-    if(user == "exit"){
-        console.log(chatHistory);
-        break;
-    }
-    
-    if(user){
-        await chat(user);
-    }
-    }
-})();
-
-
-
-async function chat( user : string){
-    const promptTemplate = await ChatPromptTemplate.fromMessages([
-    // ["system",SYSTEM_TEMPLTE], ["human","{user_input}"], ["human","{chatHistory}"]
-    ["system",SYSTEM_TEMPLTE],["human","{user_input}"],new MessagesPlaceholder("chatHistory")
-    ]);
-    
-    const promptInputValue = await promptTemplate.invoke({
-        user_input:user,
-        chatHistory : JSON.stringify(chatHistory)
+    const response = await model.invoke(promptValue);
+    // console.log(response.content);
+   
+    const promptValue2 = await summarizerToFiveLinetemplate.invoke({
+        info : response.content
     });
+    console.log("prompt value 2",promptValue2);
+    const finalSummarzizedContent = await model.invoke(promptValue2);
 
-    console.log("keerthi",promptInputValue);
+    console.log("*********************** Final Output ***********************")
+    console.log(finalSummarzizedContent.content);
 
-    const response = await model.invoke(promptInputValue);
-    console.log("Bot : ",response.content);
-    chatHistory.push({"userMessage":user,"botMessage":response});
-    const humanMessage = new HumanMessage(user);
-    const aIMessage = new AIMessage(response);
-    chatHistory.push({humanMessage,aIMessage});
-
+    
 }
 
-
+chat();
